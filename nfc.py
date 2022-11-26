10193ce569057f637c8525cfe7327c52aa736bae
xPaulFTW <lascupg13@gmail.com>
2022-11-26 05:52:52 +0200

    Update nfc.py

--- nfc.py
+++ nfc.py
@@ -14,13 +14,17 @@ def nfc_loop():
     while True:
         id, text = reader.read()
 
-        # print(id)
+        print(id, text)
 
         if id == 582238713219:
             GPIO.output(GREEN_LED, 1)
 
+            GPIO.output(RED_LED, 0)
+
         else:
             GPIO.output(RED_LED, 1)
+
+            GPIO.output(GREEN_LED, 0)
 
         record_time()
 
