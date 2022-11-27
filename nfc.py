094506d7f2902be9524d8acf8f53733c39c0a0eb
xPaulFTW <lascupg13@gmail.com>
2022-11-27 02:47:16 +0200

    added sounds

--- nfc.py
+++ nfc.py
@@ -1,4 +1,6 @@
-import mfrc522
+from mfrc522 import SimpleMFRC522
+
+from requests import get
 
 from playsound import playsound
 
@@ -10,30 +12,63 @@ from led import GPIO, record_time
 
 from vars import RED_LED, GREEN_LED
 
-reader = mfrc522.SimpleMFRC522()
+from random import randint
+
+import datetime
+
+reader = SimpleMFRC522()
 
 sound_done = 0
 
 def nfc_loop():
+    global sound_done
+
     while True:
         id, text = reader.read()
 
-        print(id, text)
+        # print(id, text)
 
-        if id == 582238713219:
+        r = get(f'https://campus.lmvineu.ro/google_login/getBilet.php?opt=tot&mac={text}')
+
+        valid = False
+
+        for row in r.json():
+            data1 = row['data1']
+            ora1 = row['ora1']
+
+            data2 = row['data2']
+            ora2 = row['ora2']
+
+            formated_date = datetime.datetime.strptime(data1 + " " + ora1, "%Y-%m-%d %H:%M:%S")
+            dt1 = datetime.datetime.timestamp(formated_date)
+
+            formated_date = datetime.datetime.strptime(data2 + " " + ora2, "%Y-%m-%d %H:%M:%S")
+            dt2 = datetime.datetime.timestamp(formated_date)
+
+            time2 = time()
+
+            if dt1 < time2 < dt2:
+                valid = True
+                
+                break
+
+        if valid == True:
             GPIO.output(GREEN_LED, 1)
 
             GPIO.output(RED_LED, 0)
+
+            if sound_done == 0:
+                Thread(target = play_asound).start()
+
+                sound_done = 1
 
         else:
             GPIO.output(RED_LED, 1)
 
             GPIO.output(GREEN_LED, 0)
 
-            global sound_done
-
             if sound_done == 0:
-                Thread(target = pl_sound).start()
+                Thread(target = play_rsound).start()
 
                 sound_done = 1
 
@@ -41,8 +76,31 @@ def nfc_loop():
 
 Thread(target = nfc_loop).start()
 
-def pl_sound():
-    playsound('warning.mp3')
+def play_asound():
+    n = randint(1, 2)
+
+    names = {
+        1: "raducanu2.mp3",
+        2: "raducanu3.mp3"
+    }
+
+    playsound(names[n])
+
+    global sound_done
+
+    sound_done = 0
+
+def play_rsound():
+    n = randint(1, 4)
+
+    names = {
+        1: "morariu1.mp3",
+        2: "morariu2.mp3",
+        3: "morariu3.mp3",
+        4: "morariu4.mp3"
+    }
+
+    playsound(names[n])
 
     global sound_done
 
